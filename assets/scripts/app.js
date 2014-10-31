f8214f7ec1ad4bd5385b8c3d2a97b113de11758a
Shaopeng Zhang <szhangpitt@gmail.com>
2014-10-30 20:53:43 -0400

    press b to toggle blurred effect

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
     setTimeout(function(){
         $('html,body').animate({scrollTop: 0}, 800);
     }, 400);
+
+    console.log('Let me know what you think... http://shaopeng.us 8-)');
+    console.log('--> Click anywhere on page and press b to toggle blurred effect. ');
+
+    $(document).on('keypress', function(e) {
+        if(e.keyCode === 98) {
+            $('.fuzzy-blurred').toggle();
+        }
+    });
     
 });
 // var SHAOPENG_LINKIEDIN_ID = 'qC72fmJGlB';
